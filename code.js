acff05f2b0fd20d516967a63ecb804015d14632a
Shahan Khan <contact@shahan.me>
2014-11-12 13:46:01 -0800

    first commit

---
+++ code.js
@@ -0,0 +1,34 @@
+/* 
+	GOAL: Create a rock paper scissors game
+	
+	Required Features:
+		- Auto correct invalid enterings:
+			i.e. enter rok instead of Rock
+		- Play the computer or another human
+*/
+// Some functions
+
+// yesNoResponse function
+// takes one parameter, response (string)
+// returns true / false (boolean) depending on the response
+var yesNoResponse = function(response) {
+	// hint: use an if statement
+
+};
+
+// Start the game
+alert("Welcome to Rock Paper Scissors!");
+
+var playComputer = prompt("Do you want to play the computer?");
+
+// play the computer
+if (yesNoResponse(playComputer)) {
+	// loop forever
+	while(...) {
+
+	}
+
+// play another player
+} else {
+
+}
